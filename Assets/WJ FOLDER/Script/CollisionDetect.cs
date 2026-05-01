c893b8a76403504c75bdfafd8eb8dbc31c666a68
ForestKum <kumweijun@gmail.com>
2026-05-02 04:32:14 +0800

    coin placement+ can help add main into here?

---
+++ Assets/WJ FOLDER/Script/CollisionDetect.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using static UnityEngine.AdaptivePerformance.Provider.AdaptivePerformanceSubsystemDescriptor;
+
+public class CollisionDetect : MonoBehaviour
+{
+    [SerializeField] GameObject thePlayer; 
+    void OnTriggerEnter (Collider other)
+    {
+        thePlayer.GetComponent(PlayerMovement).enabled = false;
+}
