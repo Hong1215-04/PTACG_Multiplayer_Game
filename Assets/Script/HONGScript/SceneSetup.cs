289c8f649753ab5f1563d9ae4b216e9413640488
LAW XING HONG <lawxinghong@gmail.com>
2026-05-08 00:28:35 +0800

    Scene_Setup
    .

---
+++ Assets/Script/HONGScript/SceneSetup.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class SceneSetup : MonoBehaviour
+{
+    [SerializeField] GameObject CameraP1;
+    [SerializeField] GameObject CameraP2;
+    [SerializeField] GameObject Player1;
+    [SerializeField] GameObject Player2;
+    [SerializeField] CamaraMovement CamMoveP1;
+    [SerializeField] CamaraMovement CamMoveP2;
+    [SerializeField] Movement MoveP1;
+    [SerializeField] Movement MoveP2;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
