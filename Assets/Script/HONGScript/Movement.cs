db85cd2ad35ea0cca7ca33ae89f89914db3946e9
Fun <shijunti438@gmail.com>
2026-05-08 04:20:16 +0800

    Update Movement.cs

--- Assets/Script/HONGScript/Movement.cs
+++ Assets/Script/HONGScript/Movement.cs
@@ -44,6 +44,11 @@ public class Movement : MonoBehaviourPun
             Vector3 horizontalMove = transform.right * horizontalInput * HoriSpeed * Time.fixedDeltaTime;
             rb.MovePosition(rb.position + forwardMove + horizontalMove);
         }
+            else
+        {
+            // 加这行 ↓
+            Debug.Log($"[Remote] 对方位置: {transform.position}，PhotonView Owner: {photonView.Owner?.NickName}");
+        }
         //Vector3 forwardMove = transform.forward * Speed * Time.fixedDeltaTime;
         //Vector3 horizontalMove = transform.right * horizontalInput * HoriSpeed * Time.fixedDeltaTime;
         //rb.MovePosition(rb.position + forwardMove + horizontalMove);
